5f65701a7c3cf85690ca4281da7ea88f72953026
Dimj1k <93049313+Dimj1k@users.noreply.github.com>
2021-11-22 20:24:50 +0300

    Add files via upload

---
+++ Python3/Метод парабол.py
@@ -0,0 +1,54 @@
+from math import sin, cos, exp, e, pi, log
+
+
+# -------------------------------------Функция-------------------------------------
+def f(x): return 2 ** x
+# -------------------------------------Функция-------------------------------------
+
+
+# Метод парабол
+# Вычисление определителя матрицы 3x3
+def det3x3(d, j, h):
+    return d[0] * j[1] * h[2] + d[1] * j[2] * h[0] + d[2] * j[0] * h[1] - d[0] * j[2] * h[1] - d[1] * j[0] * h[2] - \
+           d[2] * j[1] * h[0]
+
+
+# Интеграл параболы
+def integrate(d, j, h, o, m):
+    d = (d * (o + m) ** 3) / 3 - (d * o ** 3) / 3
+    j = (j * (o + m) ** 2) / 2 - (j * o ** 2) / 2
+    h = h * (o + m) - (h * o)
+    return d + j + h
+
+
+# Функция
+def s_fp(x1, x2, n, epsilon, s2=0):
+    cx = [1, 1, 1]
+    s1 = 0
+    x1save = x1
+    step = (x2 - x1) / n
+    while x1 < x2:
+        # Матрицы. Создание линейных уравнений
+        ax = [x1 ** 2, (x1 + step / 2) ** 2, (x1 + step) ** 2]
+        bx = [x1, (x1 + step / 2), (x1 + step)]
+        v = [f(x1), f(x1 + step / 2), f(x1 + step)]
+        # Метод Крамера
+        p = det3x3(ax, bx, cx)
+        a = det3x3(v, bx, cx) / p
+        b = det3x3(ax, v, cx) / p
+        c = det3x3(ax, bx, v) / p
+        # -----------------------
+        s1 += integrate(a, b, c, x1, step)
+        x1 = x1 + step
+    if abs(s2 - s1) <= epsilon:
+        return s1
+    else:
+        return s_fp(x1save, x2, n * 2, epsilon, s1)
+
+
+# ax^2 + bx + c = f(x)
+x0 = float(input('Введите первую координату по X: '))
+y = float(input('Введите вторую координату по X: '))
+k = 1
+acc = abs(float(input('Введите точность: ')))
+print(s_fp(x0, y, k, acc))
