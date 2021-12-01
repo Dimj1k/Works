95c0ae8cc70a7eab7bf81a32c360d5d016d0b3fa
Саратов Дмитрий <93049313+Dimj1k@users.noreply.github.com>
2021-12-01 06:00:23 +0300

    Update Task2 Lab9.py

--- Python3/Task2 Lab9.py
+++ Python3/Task2 Lab9.py
@@ -16,13 +16,19 @@ with open('Auto.txt', 'r', encoding='UTF-8') as f:
         if not '\t' in date:
             dates.append(date)
         else:
-            dates.append('1.1.1971')
+            dates.append('2.1.1970')
         copies.append(line.split('\n')[0])
 
 
 for i in range(len(dates)):
     day, month, year = dates[i].split('.')
-    age = int(((time.time() - 12 * 3600) - time.mktime(dt.date(int(year), int(month), int(day)).timetuple())) / (365.25 * 86400))
+    if int(year) < 1970:
+        year = (1970 - int(year)) + 1970
+        age = int(((time.time() - 12 * 3600) + time.mktime(dt.date(int(year), int(month), int(day)).timetuple()))
+                  / (365.25 * 86400))
+    else:
+        age = int(((time.time() - 12 * 3600) - time.mktime(dt.date(int(year), int(month), int(day)).timetuple()))
+                  / (365.25 * 86400))
     ages.append(age)
 
 
