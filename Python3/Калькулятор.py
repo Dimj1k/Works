cb9891a0e2f82ed366736ac1c50f9802c6f0c7c9
Саратов Дмитрий <93049313+Dimj1k@users.noreply.github.com>
2021-12-16 05:06:23 +0300

    Update Калькулятор.py

--- Python3/Калькулятор.py
+++ Python3/Калькулятор.py
@@ -23,7 +23,8 @@ def summ(lst):  # Сумма строк из списка
 
 
 # Раскрытие скобок
-def changing(): return re.search(re_parentheses, out).start() + 1, re.search(re_parentheses, out).end() - 1
+def changing(out):
+    return re.search(r'[(][\d+/^*-.,]+[)]', out).start() + 1, re.search(r'[(][\d+/^*-.,]+[)]', out).end() - 1
 
 
 def num_op2op(lst):  # Превращение "Число + Число" и "Число - Число" в "+" и "-" соответственно
@@ -73,21 +74,14 @@ def operator2act(lst, lstnum):  # Перебор полученных опера
             try:
                 ans = lstnum[act] / lstnum[act + 1]
             except (ZeroDivisionError, InvalidOperation):
-                print('В процессе вычислений вы поделили на ноль.\nИз-за этого будет произведён выход из программы.\n'
-                      'Ваш введенный пример сохранён в файл ZeroDivisionArithmeticExample.txt\nВыход из программы')
-                with open('ZeroDivisionArithmeticExample.txt', 'a', encoding='UTF-8') as f:
-                    f.writelines(given)
-                quit()
+                print('В процессе вычислений вы поделили на ноль')
+                main(input('Введите пример еще раз: '))
         elif lst[act] == '*':
             ans = lstnum[act] * lstnum[act + 1]
         elif lst[act] == '^':
             if lstnum[act] == 0 and lstnum[act + 1] == 0:
-                print('В процессе вычислений вы возвели 0 в степень 0, что равно неопределенности.\nИз-за этого бу'
-                      'дет произведён выход из программы. Ваш введенный пример сохранён в файл ZeroToThePowerOfZeroArit'
-                      'hmeticExample.txt\nВыход из программы')
-                with open('ZeroToThePowerOfZeroArithmeticExample.txt', 'a', encoding='UTF-8') as f:
-                    f.writelines(given)
-                quit()
+                print('В процессе вычислений вы возвели 0 в степень 0, что равно неопределенности')
+                main(input('Введите пример еще раз: '))
             else:
                 ans = lstnum[act] ** lstnum[act + 1]
         elif lst[act] == '+':
@@ -102,86 +96,83 @@ def operator2act(lst, lstnum):  # Перебор полученных опера
     return ans
 
 
-# Ввод данных
-given = input('Введите пример: ')
-given = given.replace(' ', '', given.count(' '))
+def main(given):
+    # Ввод данных
+    given = given.replace(' ', '', given.count(' '))
 
 # Регулярные выражения
-re_nums = r'[(]?[-+]?\d*[.,]?\d+[)]?|\d*[.,]?\d+'  # Числа
-re_operators = r'[/*^]|\d*[.,]?\d+[+-]|[+-]\('  # Операторы
-re_parentheses = r'[()]'  # Скобки
-re_all = re_nums + r'|' + re_operators + r'|' + re_parentheses  # Операторы + Числа + Скобки
+    re_nums = r'[(]?[-+]?\d*[.,]?\d+[)]?|\d*[.,]?\d+'  # Числа
+    re_operators = r'[/*^]|\d*[.,]?\d+[+-]|[+-]\('  # Операторы
+    re_parentheses = r'[()]'  # Скобки
+    re_all = re_nums + r'|' + re_operators + r'|' + re_parentheses  # Операторы + Числа + Скобки
 
-havenums = re.search(re_nums, given)  # Есть ли число в примере
-re_oper = r'[-+/*^]{2,}'  # Ловить больше двух операторов подряд
+    havenums = re.search(re_nums, given)  # Есть ли число в примере
+    re_oper = r'[-+/*^]{2,}'  # Ловить больше двух операторов подряд
 
 # Что нашлось из вводных данных по регулярным выражениям
-out_lst = re.findall(re_all, given)
-out = summ(out_lst)
-
-# Проверка, того что нашлось из вводных данных по регулярным выражениям с вводными данными и прочее
-while out != given or given == '' or havenums is None or given.count('(') > given.count(')') + 1 or \
-        not (re.search(re_oper, given) is None) or given.count(')') > given.count('(') or '()' in given or \
-        '()' in given + ')' or not (re.match(r'[*/^]', given) is None) or \
-        not (re.search(r'[-+*/^]$', given, re.MULTILINE) is None) or not (re.search(r'\([*/^]', given) is None) or \
-        not (re.search(r'[-+*/^]\)', given) is None):
-
-    if given == '':
-        print('Вы ввели пустой пример')
-    elif havenums is None:
-        print('Вы ввели пример без чисел')
-    elif not (re.search(re_oper, given) is None):
-        print('Вы ввели более одного операторов подряд в примере')
-    elif not (re.match(r'[*/^]', given) is None) or not (re.search(r'[-+*/^]$', given, re.MULTILINE) is None) or \
-        not (re.search(r'\([*/^]', given) is None) or not (re.search(r'[-+*/^]\)', given) is None):
-        print('Вы не дописали пример')
-    elif given.count('(') > given.count(')') + 1 or given.count(')') > given.count('('):
-        print(r'Введите равное количество "(" и ")"')
-    elif '()' in given or '()' in given + ')':
-        print('Вы ввели пустые скобки в примере')
-    elif out != given:
-        print(r'Введите пример без букв, c одной или без "." (",") в числе и иных символов отличных от "0-9", "-", "+",'
-              r' "/", "*", "^", и "(", ")"', '\nПолучено:', out)
-
-    given = again()
-    havenums = re.search(re_nums, given)
     out_lst = re.findall(re_all, given)
     out = summ(out_lst)
 
+# Проверка, того что нашлось из вводных данных по регулярным выражениям с вводными данными и прочее
+    if given == '':
+        print('Вы ввели пустой пример')
+        main(input('Введите пример еще раз: '))
+    elif havenums is None:
+        print('Вы ввели пример без чисел')
+        main(input('Введите пример еще раз: '))
+    elif not (re.search(re_oper, given) is None):
+        print('Вы ввели более одного операторов подряд в примере')
+        main(input('Введите пример еще раз: '))
+    elif not (re.match(r'[*/^]', given) is None) or not (re.search(r'[-+*/^]$', given, re.MULTILINE) is None) or \
+        not (re.search(r'\([*/^]', given) is None) or not (re.search(r'[-+*/^]\)', given) is None):
+        print('Вы не дописали пример')
+        main(input('Введите пример еще раз: '))
+    elif given.count('(') > given.count(')') + 1 or given.count(')') > given.count('('):
+        print(r'Введите равное количество "(" и ")"')
+        main(input('Введите пример еще раз: '))
+    elif '()' in given or '()' in given + ')':
+        print('Вы ввели пустые скобки в примере')
+        main(input('Введите пример еще раз: '))
+    elif out != given:
+        print(r'Введите пример без букв, c одной или без "." (",") в числе и иных символов отличных от "0-9", "-", "+",'
+              r' "/", "*", "^", и "(", ")"', '\nПолучено:', out)
+        main(input('Введите пример еще раз: '))
+
 # Добавить ")", если кол-во "(" == кол-ву ")" - 1
-if given.count('(') == given.count(')') + 1:
-    out += ')'
-    print(fr'Вы ввели "(" больше ")" на 1, пример был закрыт ")". Получено: {out}')
+    if given.count('(') == given.count(')') + 1:
+        out += ')'
+        print(fr'Вы ввели "(" больше ")" на 1, пример был закрыт ")". Получено: {out}')
 
-out = '(' + out + ')'  # Сам пример - огромная скобка
-
-# Улавливать скобки
-re_parentheses = r'[(][\d+/^*-.,]+[)]'
+    out = '(' + out + ')'  # Сам пример - огромная скобка
 
 # Вычисления внутри скобок
-j = 0
-while '(' in out:
-    j = j + 1
-    st, end = changing()
+    j = 0
+    while '(' in out:
+        j = j + 1
+        st, end = changing(out)
 
-    if not (re.search(r'[\d.,]\(', out[st - 2:st]) is None):
-        out = out[0:st - 1] + '*(' + out[st:]
-        st, end = changing()
-    if not (re.search(r'\)[\d.,]', out[end:end + 2]) is None):
-        out = out[0:end] + ')*' + out[end + 1:]
-        st, end = changing()
+        if not (re.search(r'[\d.,]\(', out[st - 2:st]) is None):
+            out = out[0:st - 1] + '*(' + out[st:]
+            st, end = changing(out)
+        if not (re.search(r'\)[\d.,]', out[end:end + 2]) is None):
+            out = out[0:end] + ')*' + out[end + 1:]
+            st, end = changing(out)
 
-    change = out[st:end].replace('--', '+', out[st:end].count('--'))
-    lstnums, lstoperators = re.findall(re_nums, change), num_op2op(re.findall(re_operators, change))
-    lstnums = str2Decimal(lstnums)
+        change = out[st:end].replace('--', '+', out[st:end].count('--'))
+        lstnums, lstoperators = re.findall(re_nums, change), num_op2op(re.findall(re_operators, change))
+        lstnums = str2Decimal(lstnums)
 
-    if len(lstnums) == 1:
-        change = lstnums[0]
-    else:
-        change = operator2act(lstoperators, lstnums)
-    out = out[0:st - 1] + str(change) + out[end + 1:]
+        if len(lstnums) == 1:
+            change = lstnums[0]
+        else:
+            change = operator2act(lstoperators, lstnums)
+        out = out[0:st - 1] + str(change) + out[end + 1:]
 
-    if '(' in out:  # Ответ
-        print(f'Убираем {j} скобку:', out[1:len(out) - 1])
-    else:
-        print('Ответ:', withount(Decimal(out)))
+        if '(' in out:  # Ответ
+            print(f'Убираем {j} скобку:', out[1:len(out) - 1])
+        else:
+            print('Ответ:', withount(Decimal(out)))
+            main(input('Введите пример: '))
+
+
+main(input('Введите пример: '))
