9a0acb1feed02859af6fb9968b60e7dc601ed0d8
Саратов Дмитрий <93049313+Dimj1k@users.noreply.github.com>
2021-12-20 17:05:55 +0300

    Update Калькулятор.py

--- Python3/Калькулятор.py
+++ Python3/Калькулятор.py
@@ -1,182 +1,344 @@
-from decimal import Decimal, InvalidOperation
-import re
+import tkinter as tk
+from random import randint
 
 
-# Функция, которая убирает .0
-def withount(a):
-    if a <= 10 ** 100 and int(a) == a:
-        return int(a)
-    else:
-        return a
-
-
-# Сумма строк из списка
-def summ(lst):
-    k = ''
-    for i in lst:
-        k += i
-    return k
-
-
-# Скобки в примере
-def changing(out):
-    return re.search(r'[(][\d+/^*-.,]+[)]', out).start() + 1, re.search(r'[(][\d+/^*-.,]+[)]', out).end() - 1
-
-
-# Превращение "Число + Число" и "Число - Число" в "+" и "-" соответственно
-def num_op2op(lst):
-    p = r'\d*[.,]?\d+|[+-]\('
+def translate(lst):
+    lst1, lstes = [], []
     for i in range(len(lst)):
-        k = lst[i]
-        if not (re.search(p, k) is None):
-            k = re.sub(p, r'', k)
-            lst.pop(i)
-            lst.insert(i, k)
-    return lst
-
-
-# Перебор полученных чисел
-def str2Decimal(lst):
-    k = []
-    for i in lst:
-        try:
-            k.append(Decimal(i))
-        except InvalidOperation:
-            if ',' in i:
-                k.append(Decimal(i.replace(',', '.')))
-    return k
-
-
-# Перебор операторов в списке
-def equal(n):
-    try: a1 = n.index('/')
-    except ValueError: a1 = float('inf')
-    try: a2 = n.index('*')
-    except ValueError: a2 = float('inf')
-    try: a3 = n.index('^')
-    except ValueError: a3 = float('inf')
-    if a1 == float('inf') and a2 == float('inf') and a3 == float('inf'):
-        try: a4 = n.index('+')
-        except ValueError: a4 = float('inf')
-        try: a5 = n.index('-')
-        except ValueError: a5 = float('inf')
-        return min([a4, a5])
-    else:
-        return min([a1, a2, a3])
-
-
-# Перебор полученных операторов
-def operator2act(lst, lstnum):
-    ans = 0
-    while len(lstnum) != 1:
-        act = equal(lst)
-        if lst[act] == '/':
+        for j in range(len(lst[i])):
             try:
-                ans = lstnum[act] / lstnum[act + 1]
-            except (ZeroDivisionError, InvalidOperation):
-                print('В процессе вычислений вы поделили на ноль')
-                return main(input('Введите пример еще раз: '))
-        elif lst[act] == '*':
-            ans = lstnum[act] * lstnum[act + 1]
-        elif lst[act] == '^':
-            if lstnum[act] == 0 and lstnum[act + 1] == 0:
-                print('В процессе вычислений вы возвели 0 в степень 0, что равно неопределенности')
-                return main(input('Введите пример еще раз: '))
-            else:
-                ans = lstnum[act] ** lstnum[act + 1]
-        elif lst[act] == '+':
-            ans = lstnum[act] + lstnum[act + 1]
-        elif lst[act] == '-':
-            ans = lstnum[act] - abs(lstnum[act + 1])
-
-        lst.pop(act)
-        lstnum.pop(act)
-        lstnum.insert(act, ans)
-        lstnum.pop(act + 1)
-    return ans
+                lst1.append(int(lst[i][j].get()))
+            except ValueError:
+                lst1.append(0)
+        lstes.append(lst1)
+        lst1 = []
+    return lstes
 
 
-# Основная функция
-def main(given):
-    # Ввод данных
-    given = given.replace(' ', '', given.count(' '))
-
-    # Регулярные выражения
-    re_nums = r'[(]?[-+]?\d*[.,]?\d+[)]?|\d*[.,]?\d+'  # Числа
-    re_operators = r'[/*^]|\d*[.,]?\d+[+-]|[+-]\('  # Операторы
-    re_parentheses = r'[()]'  # Скобки
-    re_all = re_nums + r'|' + re_operators + r'|' + re_parentheses  # Операторы + Числа + Скобки
-
-    havenums = re.search(re_nums, given)  # Есть ли число в примере
-    re_oper = r'[-+/*^]{2,}'  # Ловить больше двух операторов подряд
-
-    # Что нашлось из вводных данных по регулярным выражениям
-    out_lst = re.findall(re_all, given)
-    out = summ(out_lst)
-
-    # Проверка, того что нашлось из вводных данных по регулярным выражениям с вводными данными и прочее
-    if given == '':
-        print('Вы ввели пустой пример')
-        return main(input('Введите пример еще раз: '))
-    elif given == 'q' or given == 'Выйти' or given == 'В' or given == 'exit' or given == 'e' or given == 'quit':
-        print('Выход из программы')
-        quit()
-    elif havenums is None:
-        print('Вы ввели пример без чисел')
-        return main(input('Введите пример еще раз: '))
-    elif not (re.search(re_oper, given) is None):
-        print('Вы ввели более одного операторов подряд в примере')
-        return main(input('Введите пример еще раз: '))
-    elif not (re.match(r'[*/^]', given) is None) or not (re.search(r'[-+*/^]$', given, re.MULTILINE) is None) or \
-            not (re.search(r'\([*/^]', given) is None) or not (re.search(r'[-+*/^]\)', given) is None):
-        print('Вы не дописали пример')
-        return main(input('Введите пример еще раз: '))
-    elif given.count('(') > given.count(')') + 1 or given.count(')') > given.count('('):
-        print(r'Введите равное количество "(" и ")"')
-        return main(input('Введите пример еще раз: '))
-    elif '()' in given or '()' in given + ')':
-        print('Вы ввели пустые скобки в примере')
-        return main(input('Введите пример еще раз: '))
-    elif out != given:
-        print(r'Введите пример без букв, c одной или без "." (",") в числе и иных символов отличных от "0-9", "-", "+",'
-              r' "/", "*", "^", и "(", ")"', '\nПолучено:', out)
-        return main(input('Введите пример еще раз: '))
-
-    # Добавить ")", если кол-во "(" == кол-ву ")" - 1
-    if given.count('(') == given.count(')') + 1:
-        out += ')'
-        print(fr'Вы ввели "(" больше ")" на 1, пример был закрыт ")". Получено: {out}')
-
-    out = '(' + out + ')'  # Сам пример - огромная скобка
-
-    # Вычисления внутри скобок
-    j = 0
-    while '(' in out:
-        j = j + 1
-        st, end = changing(out)
-
-        if not (re.search(r'[\d.,]\(', out[st - 2:st]) is None):
-            out = out[0:st - 1] + '*(' + out[st:]
-            st, end = changing(out)
-        if not (re.search(r'\)[\d.,]', out[end:end + 2]) is None):
-            out = out[0:end] + ')*' + out[end + 1:]
-            st, end = changing(out)
-
-        change = out[st:end].replace('--', '+', out[st:end].count('--'))
-        lstnums, lstoperators = re.findall(re_nums, change), num_op2op(re.findall(re_operators, change))
-        lstnums = str2Decimal(lstnums)
-
-        if len(lstnums) == 1:
-            change = lstnums[0]
-        else:
-            change = operator2act(lstoperators, lstnums)
-        out = out[0:st - 1] + str(change) + out[end + 1:]
-
-        if '(' in out:  # Ответ
-            print(f'Убираем {j} скобку:', out[1:len(out) - 1])
-        else:
-            print('Ответ:', withount(Decimal(out)))
-            return main(input('Введите пример: '))
+class MatrixException(Exception):
+    pass
 
 
-main(input('Введите пример: '))
+class Matrices():  # Матрицы
+
+    def __init__(self, nA, mA, nB, mB, matricA, matricB):
+        self.mA = mA
+        self.nA = nA
+        self.nB = nB
+        self.mB = mB
+        self.matricA = matricA
+        self.matricB = matricB
+
+    @property
+    def set_mA(self):
+        return self.mA
+
+    @set_mA.setter
+    def set_mA(self, mA):
+        self.mA = mA
+
+    @property
+    def set_nA(self):
+        return self.nA
+
+    @set_nA.setter
+    def set_nA(self, nA):
+        self.nA = nA
+
+    @property
+    def set_nB(self):
+        return self.nB
+
+    @set_nB.setter
+    def set_nB(self, nB):
+        self.nB = nB
+
+    @property
+    def set_mB(self):
+        return self.mB
+
+    @set_mB.setter
+    def set_mB(self, mB):
+        self.mB = mB
+
+    @property
+    def set_matricA(self):
+        return self.matricA
+
+    @set_matricA.setter
+    def set_matricA(self, matricA):
+        self.matricA = matricA
+
+    @property
+    def set_matricB(self):
+        return self.matricB
+
+    @set_matricB.setter
+    def set_matricB(self, matricB):
+        self.matricB = matricB
+
+    @property
+    def suma(self):  # Сумма матриц А и В
+        matricCs, lstes = [], []
+        if self.nA != self.nB or self.mA != self.mB:
+            return ' Введите равное количество столбцов и строчек в матрицах А и B '
+        for i in range(len(self.matricA)):
+            for j in range(len(self.matricA[i])):
+                lstes.append(self.matricA[i][j] + self.matricB[i][j])
+            matricCs.append(lstes)
+            lstes = []
+        return matricCs
+
+    @suma.setter
+    def suma(self, matricCs):
+        self.matricCs = matricCs
+
+    @property
+    def difference(self):  # Разность матриц А и В
+        matricCd, lstes = [], []
+        if self.nA != self.nB or self.mA != self.mB:
+            return ' Введите равное количество столбцов и строчек в матрицах А и B '
+        for i in range(len(self.matricA)):
+            for j in range(len(self.matricA[i])):
+                lstes.append(self.matricA[i][j] - self.matricB[i][j])
+            matricCd.append(lstes)
+            lstes = []
+        return matricCd
+
+    @difference.setter
+    def difference(self, matricCd):
+        self.matricCd = matricCd
+
+    @property
+    def transpA(self):  # Транспонирование матрицы А
+        matricCt, lstes, i = [], [], 0
+        for j in range(self.nA):
+            while i < self.mA:
+                lstes.append(self.matricA[i][j])
+                i = i + 1
+            i = 0
+            matricCt.append(lstes)
+            lstes = []
+        return matricCt
+
+    @transpA.setter
+    def transpA(self, matricCt):
+        self.matricCt = matricCt
+
+    @property
+    def mult(self):  # Умножение матриц А и В
+        matricCm, lstes, lst = [], [], []
+        if self.nA != self.mB:
+            return 'Количество столбцов А должно равняться количеству строк В'
+        for i in range(self.mA):
+            for k in range(self.nB):
+                for j in range(self.mB):
+                    lst.append(self.matricA[i][j] * self.matricB[j][k])
+                lstes.append(sum(lst))
+                lst = []
+            matricCm.append(lstes)
+            lstes = []
+        return matricCm
+
+    @mult.setter
+    def mult(self, matricCm):
+        self.matricCm = matricCm
+
+
+class SquareMatrices(Matrices):  # Квадрат
+
+    @property
+    def traceA(self):  # След матрицы А
+        if self.nA != self.mA:
+            return 'След матрицы можно вычислить только у квадратной матрицы'
+        lst = []
+        for i in range(len(self.matricA)):
+            lst.append(self.matricA[i][i])
+        trace = sum(lst)
+        return trace
+
+    @traceA.setter
+    def traceA(self, trace):
+        self.trace = trace
+
+
+class RectangleMatrices(Matrices):  # Прямоугольник
+    pass
+
+
+class TriangleMatrices(Matrices):  # Над или под главной диагональю нули
+    pass
+
+
+class StringMatrices(Matrices):  # Строка
+    pass
+
+
+class ColumnMatrices(Matrices):  # Столбец
+    pass
+
+
+class UnitMatrices(Matrices):  # По диагонали единицы
+    pass
+
+
+class ZeroMatrices(Matrices):  # Все нули
+    pass
+
+
+class DiagonalMatrices(Matrices):  # По диагонали числа
+    pass
+
+
+# Окно
+window = tk.Tk()
+screen_width = str(int(window.winfo_screenwidth() // 1.5))
+screen_height = str(int(window.winfo_screenheight() // 1.5))
+window.title('Matrix')
+window.geometry(screen_width + 'x' + screen_height + '+' +
+                str(int(int(screen_width) * 1.5 // 5)) + '+' + str(int(int(screen_height) * 1.5 // 6)))
+try:
+    px = int(input('Введите шрифт текста (11 - по умолчанию): '))
+except:
+    px = 11
+try:
+    k = ('Times New Roman', px)
+except:
+    k = ('Times New Roman', 11)
+
+
+# Ввод данных
+frm0 = tk.LabelFrame(window, text='Ввод размерности матриц', font=k)
+frm0.grid(row=0, column=0)
+lbl0Am = tk.Label(frm0, font=k, text='Количество строк матрицы А:').pack()
+Am = tk.IntVar()
+ent0Am = tk.Spinbox(frm0, font=k, textvariable=Am, from_=1, to=float('inf')).pack()
+lbl0An = tk.Label(frm0, font=k, text='Количество столбцов матрицы А:').pack()
+An = tk.IntVar()
+ent0An = tk.Spinbox(frm0, font=k, textvariable=An, from_=1, to=float('inf')).pack()
+lbl0Bm = tk.Label(frm0, font=k, text='Количество строк матрицы B:').pack()
+Bm = tk.IntVar()
+ent0Bm = tk.Spinbox(frm0, font=k, textvariable=Bm, from_=0, to=float('inf')).pack()
+lbl0Bn = tk.Label(frm0, font=k, text='Количество столбцов матрицы B:').pack()
+Bn = tk.IntVar()
+ent0Bn = tk.Spinbox(frm0, font=k, textvariable=Bn, from_=0, to=float('inf')).pack()
+
+
+# Матрица А и B
+def show1():  # Показать
+    frm2 = tk.LabelFrame(window, text='Матрица А', font=k)
+    frm2.grid(row=0, column=1, padx=k[1])
+    frm3 = tk.LabelFrame(window, text='Матрица В', font=k)
+    frm3.grid(row=0, column=2, padx=k[1])
+    global entrs1, entrs2, entrsA, entrsB, frms
+    entrs1, entrs2, entrsA, entrsB = [], [], [], []
+
+    for i in range(Am.get()):
+        for j in range(An.get()):
+            ent1 = tk.Entry(frm2, font=k, width=4)
+            ent1.grid(row=i+1, column=j+1)
+            entrs1.append(ent1)
+        entrsA.append(entrs1)
+        entrs1 = []
+    if Bn.get() != 0 and Bm.get() != 0:
+        for i in range(Bm.get()):
+            for j in range(Bn.get()):
+                ent2 = tk.Entry(frm3, font=k, width=4)
+                ent2.grid(row=i+1, column=j+1)
+                entrs2.append(ent2)
+            entrsB.append(entrs2)
+            entrs2 = []
+    else:
+        lbl2 = tk.Label(frm3, text='Введите количество строк и количество столбцов\nв матрице В больше 0', font=k)
+        lbl2.grid()
+    btn0b.pack()
+    btn0a.destroy()
+    frms = [frm2, frm3]
+
+
+def show2():  # Показать
+    for i in frms:
+        i.destroy()
+    for i in range(len(entrsB)):
+        for j in range(len(entrsB[i])):
+            entrsB[i][j].destroy()
+    for i in range(len(entrsA)):
+        for j in range(len(entrsA[i])):
+            entrsA[i][j].destroy()
+    return show1()
+
+
+def suma():  # Сумма
+    C = Matrices(An.get(), Am.get(), Bn.get(), Bm.get(), translate(entrsA), translate(entrsB))
+    a = C.suma
+    res.set(str(a)[1:-1].replace(r"], ", '\n', str(a).count(r"], ")).replace(',', ' ', str(a).count(',')). \
+            replace(']', '', str(a).count(']')).replace('[', '', str(a).count('[')))
+
+
+def difference():  # Разность
+    C = Matrices(An.get(), Am.get(), Bn.get(), Bm.get(), translate(entrsA), translate(entrsB))
+    a = C.difference
+    res.set(str(a)[1:-1].replace(r"], ", '\n', str(a).count(r"], ")).\
+            replace(']', '', str(a).count(']')).replace('[', '', str(a).count('[')))
+
+
+def transpA():  # Транспонирование
+    C = Matrices(An.get(), Am.get(), Bn.get(), Bm.get(), translate(entrsA), translate(entrsB))
+    a = C.transpA
+    res.set(str(a)[1:-1].replace(r"], ", '\n', str(a).count(r"], ")).replace(']', '', str(a).count(']')).\
+            replace('[', '', str(a).count('[')))
+
+
+def traceA():  # След
+    C = SquareMatrices(An.get(), Am.get(), Bn.get(), Bm.get(), translate(entrsA), translate(entrsB))
+    a = C.traceA
+    if a != 'След матрицы можно вычислить только у квадратной матрицы':
+        res.set('След матрицы А = ' + str(a))
+    else:
+        res.set(str(a))
+
+
+def mult():  # Умножение А и В
+    C = Matrices(An.get(), Am.get(), Bn.get(), Bm.get(), translate(entrsA), translate(entrsB))
+    a = C.mult
+    if a != 'Количество столбцов А должно равняться количеству строк В':
+        res.set(str(a)[1:-1].replace(r"], ", '\n', str(a).count(r"], ")).replace(']', '', str(a).count(']')).\
+                replace('[', '', str(a).count('[')))
+    else:
+        res.set(a)
+
+
+def rnd():  # Случайные значения в ячейках матриц
+    for i in range(len(entrsA)):
+        for j in range(len(entrsA[i])):
+            entrsA[i][j].delete(0, 'end')
+    for i in range(len(entrsB)):
+        for j in range(len(entrsB[i])):
+            entrsB[i][j].delete(0, 'end')
+    for i in range(len(entrsA)):
+        for j in range(len(entrsA[i])):
+            entrsA[i][j].insert(0, str(randint(-99, 99)))
+    for i in range(len(entrsB)):
+        for j in range(len(entrsB[i])):
+            entrsB[i][j].insert(0, str(randint(-99, 99)))
+
+
+btnr = tk.Button(frm0, font=k, text='Случайные значения в ячейках матрицы', command=rnd).pack()
+btn0a = tk.Button(frm0, font=k, text='Получить размерность матриц', command=show1)
+btn0a.pack()
+btn0b = tk.Button(frm0, font=k, text='Получить размерность матриц', command=show2)
+
+# Кнопки вычислений
+frm1 = tk.LabelFrame(window, text='Операция', font=k)
+frm1.grid(row=1)
+res = tk.StringVar()
+btn1 = tk.Button(frm1, font=k, text='Сумма А и B', command=suma).pack()
+btn2 = tk.Button(frm1, font=k, text='Разность А и В', command=difference).pack()
+btn3 = tk.Button(frm1, font=k, text='Умножение А и В', command=mult).pack()
+btn4 = tk.Button(frm1, font=k, text='Транспонирование А', command=transpA).pack()
+btn5 = tk.Button(frm1, font=k, text='След А', command=traceA).pack()
+btn6 = tk.Button(frm1, font=k, text='Выйти из программы', command=quit).pack()
+frm4 = tk.LabelFrame(window, font=k, text='Ответ')
+frm4.grid(row=1, column=1, columnspan=5)
+lbl1 = tk.Label(frm4, font=k, textvariable=res).grid(row=0, column=0)
+window.mainloop()
