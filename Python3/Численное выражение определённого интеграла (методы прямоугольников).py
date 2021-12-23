5a6d867e8882217b24faff15c279688cd63e5e50
Саратов Дмитрий <93049313+Dimj1k@users.noreply.github.com>
2021-12-23 07:13:52 +0300

    Update Численное выражение определённого интеграла (методы прямоугольников).py

--- Python3/Численное выражение определённого интеграла (методы прямоугольников).py
+++ Python3/Численное выражение определённого интеграла (методы прямоугольников).py
@@ -7,47 +7,47 @@ def y(x): return 2 * x
 
 
 # Метод левых прямоугольников
-def s_fl(a, b, n, epsilon, s2=0):
+def s_fl(a, b, n, epsilon, s2=float('inf')):
     k = abs((b - a) / n)
     s1 = 0
     for i in range(0, int(k)):
         s1 += (y(a + i * n) * n)
     s1 += (k - int(k)) * n * (y(a + int(k) * n))
-    if abs(s2 - s1) <= epsilon and s1 != 0:
+    if abs(s2 - s1) <= epsilon:
         return s1
     else:
         return s_fl(a, b, n / 2, epsilon, s1)
 
 
 # Метод средних прямоугольников
-def s_fc(a, b, n, epsilon, s2=0):
+def s_fc(a, b, n, epsilon, s2=float('inf')):
     k = abs((b - a) / n)
     s1 = 0
     for i in range(1, int(k) + 1):
         s1 += (y(a + (2 * i - 1) * n / 2) * n)
     if int(k) != k:
         s1 += (y(((b - (a + int(k) * n)) / 2) + (a + int(k) * n)) * (b - (a + int(k) * n)))
-    if abs(s2 - s1) <= epsilon and s1 != 0:
+    if abs(s2 - s1) <= epsilon:
         return s1
     else:
         return s_fc(a, b, n / 2, epsilon, s1)
 
 
 # Метод правых прямоугольников
-def s_fr(a, b, n, epsilon, s2=0):
+def s_fr(a, b, n, epsilon, s2=float('inf')):
     k = abs((b - a) / n)
     s1 = 0
     for i in range(0, int(k)):
         s1 += (y(b - i * n) * n)
     s1 += (k - int(k)) * n * (y(b - int(k) * n))
-    if abs(s2 - s1) <= epsilon and s1 != 0:
+    if abs(s2 - s1) <= epsilon:
         return s1
     else:
         return s_fr(a, b, n / 2, epsilon, s1)
 
 
 # Метод левых и правых прямоугольников с точки х0
-def s_fm(a, b, c, n, epsilon, s2=0):
+def s_fm(a, b, c, n, epsilon, s2=float('inf')):
     k1 = abs((c - b) / n)
     k2 = abs((c - a) / n)
     s1 = 0
@@ -56,7 +56,7 @@ def s_fm(a, b, c, n, epsilon, s2=0):
     for i in range(0, int(k2)):
         s1 += (y(c - i * n) * n)
     s1 += (k1 - int(k1)) * n * (y(c + int(k1) * n)) + (k2 - int(k2)) * n * (y(c - int(k1) * n))
-    if abs(s2 - s1) <= epsilon and s1 != 0:
+    if abs(s2 - s1) <= epsilon:
         return s1
     else:
         return s_fm(a, b, c, n / 2, epsilon, s1)
