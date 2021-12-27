6eab3018b8aba9cc22df60e2eda566f89181d7c4
Саратов Дмитрий <93049313+Dimj1k@users.noreply.github.com>
2021-12-27 14:25:33 +0300

    Update Матричный калькулятор.py

--- Python3/Матричный калькулятор.py
+++ Python3/Матричный калькулятор.py
@@ -667,17 +667,19 @@ canva.pack(side='top')
 
 def project2x2():  # Параллелограмм матрицы на Canvas
     try:
-        a, b, c = abs(int(entrsA[0][0].get())), abs(int(entrsA[1][0].get())), abs(int(entrsA[0][1].get()))
-        d = abs(int(entrsA[1][1].get()))
-        clr = f'#{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}'\
+        # for i in range(1000):
+        #     rnd()
+            a, b, c = abs(int(entrsA[0][0].get())), abs(int(entrsA[1][0].get())), abs(int(entrsA[0][1].get()))
+            d = abs(int(entrsA[1][1].get()))
+            clr = f'#{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}'\
               f'{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}'
-        activeclr = f'#{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}'\
+            activeclr = f'#{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}'\
                     f'{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}{str(hex(randint(0, 15)))[2]}'
-        canva.create_line(0, 0, a, b, fill=clr, width=k[1] // 2, activefill=activeclr)
-        canva.create_line(a, b, a + c, b + d, fill=clr, width=k[1] // 2, activefill=activeclr)
-        canva.create_line(a + c, b + d, c, d, fill=clr, width=k[1] // 2, activefill=activeclr)
-        canva.create_line(c, d, 0, 0, fill=clr, width=k[1] // 2, activefill=activeclr)
-        print('Рисую параллелограмм в Canvas')
+            canva.create_line(0, 0, a, b, fill=clr, width=k[1] // 2, activefill=activeclr)
+            canva.create_line(a, b, a + c, b + d, fill=clr, width=k[1] // 2, activefill=activeclr)
+            canva.create_line(a + c, b + d, c, d, fill=clr, width=k[1] // 2, activefill=activeclr)
+            canva.create_line(c, d, 0, 0, fill=clr, width=k[1] // 2, activefill=activeclr)
+            print('Рисую параллелограмм в Canvas')
     except NameError:
         res.set('Введите размерность матриц')
     except IndexError:
