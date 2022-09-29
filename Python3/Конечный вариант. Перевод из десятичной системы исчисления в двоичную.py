29d43f58f91b314b6b76a50f49e6e101edc08164
Саратов Дмитрий <93049313+Dimj1k@users.noreply.github.com>
2022-09-29 10:17:35 +0300

    Update Конечный вариант. Перевод из десятичной системы исчисления в двоичную.py

--- Python3/Конечный вариант. Перевод из десятичной системы исчисления в двоичную.py
+++ Python3/Конечный вариант. Перевод из десятичной системы исчисления в двоичную.py
@@ -1,51 +1,49 @@
 from decimal import Decimal, getcontext
+
 getcontext().prec = 2 ** 16
 
-
-n=Decimal(input())
-n1=n-n//1
-n=int(n)
-k=''
-p=[]
-F=0
-o1=0
-o2=0
-
+n = Decimal(input())
+n1 = n - n // 1
+n = int(n)
+k = ''
+p = []
+F = 0
+o1 = 0
+o2 = 0
 
 if n >= 1:
-	while n >= 1:
-		k=k+str(n%2)
-		n=n//2
+    while n >= 1:
+        k = k + str(n % 2)
+        n = n // 2
 else:
-	k='0'
+    k = '0'
 
-
-k=k[::-1]
-we=len(k)
+k = k[::-1]
+we = len(k)
 if n1 > 0:
-    while F!=1:
+    while F != 1:
         p.append(n1)
-        n1=n1*2
+        n1 = n1 * 2
         if n1 >= 1:
-            k=k+'1'
-            n1=n1-1
-        else: k=k+'0'
-        j=len(p)
-        ji=len(set(p))
+            k = k + '1'
+            n1 = n1 - 1
+        else:
+            k = k + '0'
+        j = len(p)
+        ji = len(set(p))
         if j != ji:
-            for i in range(0,j-1):
-                F=1
-                if p[i]==p[j-1]:
-                    o1=i
-                    o2=j-1
-
+            for i in range(0, j - 1):
+                F = 1
+                if p[i] == p[j - 1]:
+                    o1 = i
+                    o2 = j - 1
 
 if o1 > 0 or o2 > 0:
-	o1=o1+we
-	o2=o2+we
-	if n1 != 0:
-	    print(k[0:we]+'.'+k[we:o1]+'('+k[o1:o2]+')')
-	else:
-	    print(k[0:we]+'.'+k[we:o1])
+    o1 = o1 + we
+    o2 = o2 + we
+    if n1 != 0:
+        print(k[0:we] + '.' + k[we:o1] + '(' + k[o1:o2] + ')')
+    else:
+        print(k[0:we] + '.' + k[we:o1])
 else:
-	print(k)
+    print(k)
